5da534ff4d0c6de431129187c6b3a9f9ca517636
Gabriel Lima <gabrielpk1@live.com>
2022-01-13 18:11:53 -0300

    add pricingCards

---
+++ src/components/PriceCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+interface cardProps {
+  type: string;
+  content: Array<string>;
+  text: string | undefined;
+}
+
+export default function PriceCard({ type, content, text }: cardProps) {
+  const createContent = (text: string, key: number) => (
+    <p key={key} className="price-content">
+      <i style={{ color: '#00da91' }} className="fas fa-check"></i>
+      {text}
+    </p>
+  );
+
+  return (
+    <div className="plan-container">
+      <div className={`price-card ${text ? 'white' : ''}`}>
+        <div className="price-type">
+          <img className="price-logo" src="logo192.png" alt="Logo HubLocal" />
+          <span className="logo-text">Hub</span>
+          {type}
+        </div>
+        <div className="content-container">
+          {content.map((text, index) => createContent(text, index))}
+        </div>
+        {text ? <div className="text-container">{text}</div> : null}
+      </div>
+      <a
+        href="http://hublocal.com.br/planos/#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6IjE5MjYiLCJ0b2dnbGUiOmZhbHNlfQ%3D%3D"
+        className={`price-btn ${text ? 'green' : ''}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Consultar Preço
+      </a>
+    </div>
+  );
+}
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import '../styles/Pricing.css';
+import PriceCard from './PriceCard';
 
 export default function Pricing() {
-  return <section className="pricing"></section>;
+  return (
+    <section className="pricing">
+      <div className="header-container">
+        <h2 className="section-header">Os Nossos Planos</h2>
+      </div>
+      <div className="princing-container">
+        <PriceCard
+          type="Start"
+          content={['CADASTRO MAPAS E LISTAS', 'ACESSO AO GERENCIADOR']}
+          text={undefined}
+        />
+        <PriceCard
+          type="Standard"
+          content={[
+            'TUDO DO PLANO START',
+            'OPERADOR DE SUPORTE',
+            'ATUALIZAÇÃO SEMANAL',
+            'PUBLICAÇÃO DE CONTEÚDOS',
+            'GESTÃO DE AVALIAÇÕES',
+          ]}
+          text={undefined}
+        />
+        <PriceCard
+          type="Premium"
+          content={[
+            'TUDO DO PLANO STANTARD',
+            'REGISTRO DE CHAMADAS',
+            'GRAVAÇÃO DE LIGAÇÕES',
+            'ALERTAS DE LIGAÇÕES PERDIDAS',
+            'REGISTRO DE ROTAS',
+            'MAPA DE ORIGEM DE ROTAS',
+            'BUSINESS INTELLIGENCE',
+          ]}
+          text={undefined}
+        />
+        <PriceCard
+          type="Enterprise"
+          content={['PLANO CUSTOMIZÁVEL']}
+          text="Ideal para rede de empresas com múltiplas unidades de negócios e pontos de vendas."
+        />
+      </div>
+    </section>
+  );
 }
