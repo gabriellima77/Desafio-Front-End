bba4592fec5a7470def68f5675a6063e4d2cf483
Gabriel Lima <gabrielpk1@live.com>
2022-01-12 21:53:50 -0300

    compact images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,25 +14,25 @@ export default function About() {
 
       <div className="cards-container">
         <CardItem
-          src="images/66260.jpg"
+          src="images/presenca.jpg"
           text="Verifique a presença da sua empresa nos mapas e listas da internet de forma otimizada."
           alt="Análise de Presença"
           index="01"
         />
         <CardItem
-          src="images/20944035.jpg"
+          src="images/correcao.jpg"
           text="A HubLocal usará as funcionalidades de correção automatizada, mantendo assim as informações do seu negócio sempre completas e atualizadas."
           alt="Correção de Presença"
           index="02"
         />
         <CardItem
-          src="images/19198945.jpg"
+          src="images/manutencao.jpg"
           text="Com as funcionalidades de divulgação da HubLocal, fica fácil divulgar promoções, cupons e campanhas regionais de sua empresa."
           alt="Manutenção de Presença"
           index="03"
         />
         <CardItem
-          src="images/analyze.jpg"
+          src="images/analise.jpg"
           text="Com os relatórios de inteligência de mercado da HubLocal, você irá descubrir quais são as suas melhores fontes de novos clientes."
           alt="Análise de Resultados"
           index="04"
